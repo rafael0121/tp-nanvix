ac29215808c1eb2b138670dd9b3f6974cc5c3d59
root <root@PC-ARTHUR-LANNA>
2023-09-01 20:00:47 -0300

    Criação da Função TRAP

---
+++ src/lib/libc/sys/get_process_info/get_process_info.c
@@ -0,0 +1,25 @@
+#include <nanvix/syscall.h>
+#include <nanvix/pm.h>
+#include <errno.h>
+
+int get_process_info(pid_t pid, struct process_buf *buf)
+{
+	int ret;
+
+	__asm__ volatile
+	(
+		"int $0x80"
+		: "=a" (ret)
+		: "0" (NR_get_process_info),
+		  "b" (pid),
+		  "c" (buf) 
+	);
+
+	if (ret < 0)
+	{
+		errno = -ret;
+		return -1;
+	}
+	
+	return(ret);
+}
