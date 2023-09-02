86debfe7fea606b6d5a07306c650752f96a69580
Rafael <rafaelramosdeandrade20@gmail.com>
2023-09-01 22:13:50 -0300

    Arch Script

---
+++ tools/dev/setup/arch.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# If you are using arch distribution use this script
+# Update and install package from pacman repository
+
+pacman -Syu --noconfirm \
+	bison \
+	base-devel \
+	clang \
+	doxygen \
+	flex \
+	gcc \
+	gdb \
+	cdrtools \
+	graphviz \
+	grub \
+	ncurses \
+	glibmm-2.68 \
+	gmp \
+	pixman \
+	sdl2 \
+	make \
+	mtools \
+	ninja \
+	pkgconf \
+	texinfo \
+	libisoburn \
+	xz \
