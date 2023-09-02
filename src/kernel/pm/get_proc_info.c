ddbe07dfdff2580e78851130e6492ff058d50292
Rafael <rafaelramosdeandrade20@gmail.com>
2023-09-01 21:27:28 -0300

    Correcao Sintaxe

--- src/kernel/pm/get_proc_info.c
+++ src/kernel/pm/get_proc_info.c
@@ -1,11 +1,12 @@
 #include <nanvix/pm.h>
 
-\*
+/*
  * @brief Função de sistema, procura se o processo como o PID informado existe e chama a função do_get_process_info
  * @param pid Identificador do processo
  * @param buf Ponteiro para struct com informações do processo
  * @returns 0 para processo encontrado e -1 para não encontrado
-\*
+*/
+
 int sys_get_process_info(pid_t pid, struct process_buf *buf)
 {	
 	struct process *proc; 
