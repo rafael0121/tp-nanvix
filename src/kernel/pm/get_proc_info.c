89c84ffe7f88e4ddd8fdf6be0f6071d9cd326211
Rafael <rafaelramosdeandrade20@gmail.com>
2023-09-14 10:27:17 -0300

    [FIX] Security Fix: Verifica se programa possui espaco alocado na memoria antes de escrever

--- src/kernel/pm/get_proc_info.c
+++ src/kernel/pm/get_proc_info.c
@@ -1,4 +1,5 @@
 #include <nanvix/pm.h>
+#include <nanvix/mm.h>
 
 /*
  * @brief Função de sistema, procura se o processo como o PID informado existe e chama a função do_get_process_info
@@ -10,12 +11,21 @@
 int sys_get_process_info(pid_t pid, struct process_buf *buf)
 {	
 	struct process *proc; 
-	
+		
 	for (proc = FIRST_PROC-1; proc <= LAST_PROC; proc++)
 	{
+		if(!IS_VALID(proc)){
+			continue;
+		};
+
+
 		if (pid == proc->pid)
 		{
+			if (!chkmem(buf, sizeof(proc), MAY_WRITE))
+				return (-1);
+			
 			do_get_process_info(proc, buf);	
+
 			return 0;
 		}
 	}
