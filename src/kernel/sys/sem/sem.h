41bcdc45d01a7ad7fd07c7cc1d365eba5cfd7041
Rafael <rafaelramosdeandrade20@gmail.com>
2023-11-06 09:15:56 -0300

    feat: add define from nanvix/sem.h

--- src/kernel/sys/sem/sem.h
+++ src/kernel/sys/sem/sem.h
@@ -1,5 +1,9 @@
 #ifdef SEM_H_
 #define SEM_H_
+   	
+    #define GETVAL   0 /**< Returns the value of a semaphore. */
+	#define SETVAL   1 /**< Sets the value of a semaphore.    */
+	#define IPC_RMID 3 /**< Destroys a semaphore.            */
 
     EXTERN int sys_semget(unsigned key);
     EXTERN int sys_semop(int semid, int semop);
