70affa090a021fae2e299394757444326b8a4d96
Rafael <rafaelramosdeandrade20@gmail.com>
2023-11-10 16:35:06 -0300

    refactor: criação do semáforo

--- src/kernel/sys/sem/semget.c
+++ src/kernel/sys/sem/semget.c
@@ -1,81 +1,42 @@
 #include <nanvix/const.h>
 #include <sys/sem.h>
 
-
 /**
  *  @brief Cria um novo semáforo.
  */
-int create_semaphore(key){
-    
-    // Cria um novo semáforo. 
-    struct semaphore *new_sem = malloc(sizeof(struct semaphore));
+int create_semaphore(unsigned key)
+{
+    struct semaphore *newsem = NULL;
 
-    // Inicializa as variáveis do novo semáforo.
-    new_sem->key = key;
-    new_sem->value = 1;
-    new_sem->assoc_list.root->proc;
-    new_sem->assoc_list.root->next;
-    new_sem->next = NULL;
-    
-    // Adiciona o semáforo no final da lista encadeada.
-    sem_list.tail->next = new_sem;
-    sem_list.tail = new_sem;
-    size++;
-    
-    //Chama a função para associar o processo atual ao semáforo.
-    associate_semaphore(new_sem);
-
-    return 0;
-}
-
-/**
- *  @brief Associa um processo a um semáforo.
- */
-int associate_semaphore(struct semaphore *sem){
-    
-    // Criação de um ponteiro auxiliar.
-    struct node_proc *ptr = sem.assoc_list.root;
-    
-    // Busca se o processo atual já está associado ao semáforo.
-    while (ptr!=NULL) {
-        if (ptr == curr_proc) {
-            return 0;
+    for (int i = 0; i < SEM_MAX; i++){
+        if (semtab[i].state == INACTIVE){
+            newsem = semtab[i];
+            break;
         }
-        ptr = ptr->next;
     }
-    
-    /* Associa o processo atual ao semáforo. */
 
-    // Cria e inicializa novo nó da fila de processos associados.
-    struct node_proc *new = malloc(sizeof(node_proc));
-    new->proc = curr_proc;
-    new->next == NULL;
-
-    // Associa o novo nó ao final da fila de processos associados.
-    ptr = sem.assoc_list.tail;
-    sem.assoc_list.tail = new;
-    ptr->next = new;
+    if (newsem == NULL)
+        return -1;
     
+    newsem.key = key;
+    newsem.value = 1;
+    newsem.state = ACTIVE;
+
+    return newsem.id;
 }
 
 /**
- * @brief Cria ou associa um processo a um semáforo.
+ * @brief Cria e/ou retorna o key de um semáforo ativo.
  */
 PUBLIC int sys_semget(sem_t key)
 {   
-    // Criação de um ponteiro auxiliar.
-    struct semaphore *sem = sem_list.root;
-    
-    // Verifica se já existe um processo com essa key.
-    while(sem != NULL){
-        if(sem->key == key){
-            associate_semaphore(sem);
-            return 0;
-        }
-        sem = sem->next;
+    // Busca o semáforo com o key informado.
+    for (int i = 0; i < SEM_MAX; i++) {
+        if (semtab[i].state == ACTIVE){ 
+            if (semtab[i].key == key)
+                return key;
     }
-    
-    create_semaphore(key);
-    
-    return 0;
+
+    // Cria um semáforo.
+    return create_semaphore(key);
 }
