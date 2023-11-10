a7625b8e1767360193872a8e211931cb82484fdc
Rafael <rafaelramosdeandrade20@gmail.com>
2023-11-10 16:34:29 -0300

    refactor: reimplementação da lógica dos semáforos

--- src/kernel/sys/sem.c
+++ src/kernel/sys/sem.c
@@ -1,17 +1,12 @@
-#include <sem/sem.h>
-#include <stdlib.h>
+#include <sys/sem.h>
 
-EXTERN struct proc_assoc_list assoc_list;
-EXTERN struct semaphore_list sem_list;
+PUBLIC struct semaphore semtab[SEM_MAX];
 
 void sem_init()
 {
-    //Inicia lista de semáforo.
-    sem_list.root = malloc(sizeof(struct semaphore));
-    sem_list.root.key = 0;
-    sem_list.root.value = 0;
-    sem_list.root.next = NULL;
-    
-    sem_list.tail = sem_list.root;
-    sem_list.size = 0;
+    // 
+    for (int i=0; i<SEM_MAX; i++) {
+        semtab[i].state = INACTIVE;
+        semtab[i].id = i;
+    }
 }
