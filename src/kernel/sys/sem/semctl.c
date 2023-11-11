20675a04684806b81891e815d7e83f53cf4869bd
Rafael <rafaelramosdeandrade20@gmail.com>
2023-11-10 21:43:51 -0300

    feat: criação dos protótipos das funções

--- src/kernel/sys/sem/semctl.c
+++ src/kernel/sys/sem/semctl.c
@@ -1,7 +1,44 @@
 #include <nanvix/const.h>
 #include <sys/sem.h>
 
-PUBLIC int sys_semctl(int semid, int cmd, int val){
+/**
+ * @brief: Retorna o valor corrente do semáforo
+ */
+int getval (int semid)
+{
 
-    return 0;
+
+}
+
+/**
+ * @brief: Define o valor do semáforo como sendo val.
+ */
+int setval (int semid, int val){
+
+
+}
+
+/**
+ * @brief: Retorna o valor corrente do semáforo
+ */
+int ipc_rmid (int semid)
+{
+    
+
+}
+
+/**
+ * @brief Executa uma série de operações no semáforo
+ *        definidas pela variável cmd.
+ */
+PUBLIC int sys_semctl (int semid, int cmd, int val)
+{
+    switch (cmd) {
+        case GETVAL:
+            return getval(semid);
+        case SETVAL:
+            return setval(semid, val);
+        case IPC_RMID:
+            return ipc_rmid(semid);
+    }
 }
