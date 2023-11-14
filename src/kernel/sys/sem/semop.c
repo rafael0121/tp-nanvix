62642123450d24391765d18937a0d8435caee129
Marcus <marcusleandro12357@gmail.com>
2023-11-14 19:15:14 -0300

    setval implementado

--- src/kernel/sys/sem/semop.c
+++ src/kernel/sys/sem/semop.c
@@ -1,7 +1,22 @@
 #include <nanvix/const.h>
 #include <sys/sem.h>
 
+
 PUBLIC int sys_semop(int semid, int semop){
-    
+
+    struct semaphore *sem = NULL;
+
+    // Busca o semáforo com o id informado.
+    for (int i = 0; i < SEM_MAX; i++) {
+        if (semtab[i].state == ACTIVE) 
+            if (semtab[i].id == semid) {
+                sem = &semtab[i];
+                break;
+            }
+    }
+
+    if(sem == NULL) {
+        return -1;
+    } 
 
 }
