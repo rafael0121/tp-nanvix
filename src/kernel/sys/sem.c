cdd48f1f34292b477666a5ef05756ea9a7101fae
rafael <rafaelramosdeandrade20@gmail.com>
2023-11-14 15:56:12 -0300

    feat: inicializa a estrutura de semáforos

--- src/kernel/sys/sem.c
+++ src/kernel/sys/sem.c
@@ -4,9 +4,10 @@ PUBLIC struct semaphore semtab[SEM_MAX];
 
 void sem_init()
 {
-    // 
+    // Inicializa a tabela de semáforos.
     for (int i=0; i<SEM_MAX; i++) {
         semtab[i].state = INACTIVE;
         semtab[i].id = i;
     }
 }
+
