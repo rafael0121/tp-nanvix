622c02e77e98f17140227b0af16b51d3db83af27
rafael <rafaelramosdeandrade20@gmail.com>
2023-11-14 21:41:07 -0300

    feat: criação das funções up e down

--- src/kernel/sys/sem/semop.c
+++ src/kernel/sys/sem/semop.c
@@ -1,7 +1,24 @@
 #include <nanvix/const.h>
 #include <sys/sem.h>
 
-PUBLIC int sys_semop(int semid, int semop){
-    
+int up (int semid)
+{
 
+    
+}
+
+int down (int semid)
+{
+
+}
+
+PUBLIC int sys_semop(int semid, int op){
+
+    if(op > 0)
+        return up(semid);       
+    
+    if(op < 0)
+        return down(semid);
+    
+    return -1;
 }
